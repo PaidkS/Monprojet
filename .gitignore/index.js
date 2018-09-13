1c9eace1ae0cf7a41548c67129cee3b54daa987d
PaidkS <43180613+PaidkS@users.noreply.github.com>
2018-09-13 18:19:07 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,23 +1,14 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
-var prefix = ("s!")
-
-bot.on('ready', function() {
-    bot.user.setGame("Command: s!help");
-    console.log("Connected");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-bot.login(process.env.TOKEN);
-
-
-bot.on('message',message => {
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes du SolusBot: \n -s!help");
-    }
-
-    if (message.content === "Salut"){
-        message.reply("Bonjour");
-        console.log("commande Salut effectué");
-    }
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
+
+client.login(process.env.TOKEN);
