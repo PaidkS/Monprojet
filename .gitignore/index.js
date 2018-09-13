1c54e83671187b07d75cd4e2f45cb61e8f18f03b
PaidkS <43180613+PaidkS@users.noreply.github.com>
2018-09-13 17:49:52 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready', function() {
+    bot.user.setGame("Command: s!help");
+    console.log("Connectedç");
+});
+
+bot.login(process.env.TOKEN);
+
+
+bot.on('message',message => {
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes du SolusBot: \n -s!help");
+    }
+
+    if (message.content === "Salut"){
+        message.reply("Bonjour");
+        console.log("commande Salut effectué");
+    }
+});
