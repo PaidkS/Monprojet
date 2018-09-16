203bb716730df890d66826fed7bd4ddeefcd9546
PaidkS <43180613+PaidkS@users.noreply.github.com>
2018-09-16 14:42:17 +0200

    cwc

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -26,4 +26,19 @@ bot.on('message',message => {
     if (message.content === prefix + "paidks"){
         message.reply("It's my dady");
     }
+    
+    if (message.content === prefix + "help"){ 
+            var embed = new DiscordEmbed() 
+                .setTitle("Help") 
+                .setDescription("Les commandes du bot son") 
+                .addField("s!help","Page d'aide",true) 
+                .addField("s!beta","Informations sur le l'avancement de la beta",true) 
+                .addField("s!meme","Même aléatoire",true) 
+                .addField("s!level","Affiche votre niveau",true) 
+                .addField("s!paidks","Le meilleur",true) 
+                .addField("s!solus","Informations sur le jeux",true) 
+                .setColor("0xFF0000") 
+                .setFooter("Enjoy") 
+            message.channel.sendEmbed(embed); 
+    }
 }); 
