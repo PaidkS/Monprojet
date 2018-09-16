902b7a5497ca16cbed23b5a1083ea538e4fc36fb
PaidkS <43180613+PaidkS@users.noreply.github.com>
2018-09-16 14:59:42 +0200

    change mode

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -20,9 +20,7 @@ bot.on('message',message => {
         message.reply("La beta est en dévelopement");
         console.log("commande Salut effectué");
     }
-});
 
-bot.on('message',message => {
     if (message.content === prefix + "paidks"){
         message.reply("It's my dady");
     }
@@ -31,14 +29,14 @@ bot.on('message',message => {
             var embed = new DiscordEmbed() 
                 .setTitle("Help") 
                 .setDescription("Les commandes du bot son") 
-                .addField("s!help","Page d'aide",true) 
-                .addField("s!beta","Informations sur le l'avancement de la beta",true) 
-                .addField("s!meme","Même aléatoire",true) 
-                .addField("s!level","Affiche votre niveau",true) 
-                .addField("s!paidks","Le meilleur",true) 
-                .addField("s!solus","Informations sur le jeux",true) 
+                .addField("s!help","Page d'aide", true) 
+                .addField("s!beta","Informations sur le l'avancement de la beta", true) 
+                .addField("s!meme","Même aléatoire", true) 
+                .addField("s!level","Affiche votre niveau", true) 
+                .addField("s!paidks","Le meilleur", true) 
+                .addField("s!solus","Informations sur le jeux", true) 
                 .setColor("0xFF0000") 
                 .setFooter("Enjoy") 
-            message.reply(embed); 
+            message.channel.sendembed(embed); 
     }
 }); 
