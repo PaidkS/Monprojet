1f3a30d258ab3da1a8434aec49ebba84c6598199
PaidkS <43180613+PaidkS@users.noreply.github.com>
2018-09-16 15:03:44 +0200

    sdqf

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -26,17 +26,17 @@ bot.on('message',message => {
     }
     
     if (message.content === prefix + "help"){ 
-            var embed = new DiscordEmbed() 
-                .setTitle("Help") 
-                .setDescription("Les commandes du bot son") 
-                .addField("s!help","Page d'aide", true) 
-                .addField("s!beta","Informations sur le l'avancement de la beta", true) 
-                .addField("s!meme","Même aléatoire", true) 
-                .addField("s!level","Affiche votre niveau", true) 
-                .addField("s!paidks","Le meilleur", true) 
-                .addField("s!solus","Informations sur le jeux", true) 
-                .setColor("0xFF0000") 
-                .setFooter("Enjoy") 
-            message.channel.sendembed(embed); 
+        var embed = new DiscordEmbed() 
+            .setTitle("Help") 
+            .setDescription("Les commandes du bot son") 
+            .addField("s!help","Page d'aide", true) 
+            .addField("s!beta","Informations sur le l'avancement de la beta", true) 
+            .addField("s!meme","Même aléatoire", true) 
+            .addField("s!level","Affiche votre niveau", true) 
+            .addField("s!paidks","Le meilleur", true) 
+            .addField("s!solus","Informations sur le jeux", true) 
+            .setColor("0xFF0000") 
+            .setFooter("Enjoy") 
+        message.channel.sendembed(embed); 
     }
 }); 
