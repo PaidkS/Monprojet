e269300d75e18c1c79510d417579a893d91aa8ff
PaidkS <43180613+PaidkS@users.noreply.github.com>
2018-09-16 15:25:06 +0200

    p

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -30,11 +30,6 @@ bot.on('message',message => {
             .setTitle("Help") 
             .setDescription("Les commandes du bot son") 
             .addField("s!help","Page d'aide", true) 
-            //.addField("s!beta","Informations sur le l'avancement de la beta", true) 
-            //.addField("s!meme","Même aléatoire", true) 
-            //.addField("s!level","Affiche votre niveau", true) 
-            //.addField("s!paidks","Le meilleur", true) 
-            //.addField("s!solus","Informations sur le jeux", true) 
             .setColor("0xFF0000") 
             .setFooter("Enjoy") 
         message.channel.sendembed(embed); 
