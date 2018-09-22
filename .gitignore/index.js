4d910c49b143749ffa26d5d1d87a7b15998d142d
PaidkS <43180613+PaidkS@users.noreply.github.com>
2018-09-22 14:47:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,14 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+//Pour les xp
+const low = require('lowdb');
+const FileSync = require ('lowdb/adapters/fileSync')
+const client = new Discord.Client();
 
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({histoires: [], xp: []}).write() 
 var prefix = ("s!")
 
 bot.on('ready', function() {
@@ -40,32 +48,31 @@ bot.on('message',message => {
         message.channel.sendEmbed(embed);
     }
     
-    var msgauthor = message.author.id;
-    
+   var msgauthor = message.author.id;
+
     if(message.author.bot)return;
-    
+
     if(!db.get("xp").find({user: msgauthor}).value()){
         db.get("xp").push({user: msgauthor, xp: 1}).write();
-    ]
-    else{
-        var userdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+    }
+    else    {
+        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
         console.log(userxpdb);
         var userxp = Object.values(userxpdb)
         console.log(userxp);
-        console.log(`Nombre d'xp : ${usercp[1]}`)
-    
-        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
-           
+        console.log(`Nombre d'xp : ${userxp[1]}`)
+         db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
     }
-               
+
     if (message.content === prefix + "level"){
-        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
-        var xpfinal = Object.value("xp");
-        var xp_embed = Discord.RichEmbed()
-            .setTitle("Xp de ${message.author.username}")
+        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value();
+        var xpfinal = Object.values(xp);
+        var xp_embed = new Discord.RichEmbed()
+            .setTitle(`Xp de ${message.author.username}`)
             .setDescription("Voici Ton XP !")
-            .addField("XP :", '${xpfinal[1]} xp')
-        message.channel.send{(embed: xp_embed});
+            .addField("XP :",   `${xpfinal[1]} xp`)
+        message.channel.send({embed: xp_embed});
+
     }
                      
 }); 
