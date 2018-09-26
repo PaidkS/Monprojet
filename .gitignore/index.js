f3808988716b0ca48f296fc9158626dca9eca37c
PaidkS <43180613+PaidkS@users.noreply.github.com>
2018-09-26 20:45:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -47,32 +47,5 @@ bot.on('message',message => {
         message.channel.sendEmbed(embed);
     }
     
-   var msgauthor = message.author.id;
-
-    if(message.author.bot)return;
-
-    if(!db.get("xp").find({user: msgauthor}).value()){
-        db.get("xp").push({user: msgauthor, xp: 1}).write();
-    }
-    else    {
-        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
-        console.log(userxpdb);
-        var userxp = Object.values(userxpdb)
-        console.log(userxp);
-        console.log(`Nombre d'xp : ${userxp[1]}`)
-         db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
-    }
-
-    if (message.content === prefix + "level"){
-        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value();
-        var xpfinal = Object.values(xp);
-        var xp_embed = new Discord.RichEmbed()
-            .setTitle(`Xp de ${message.author.username}`)
-            .setDescription("Voici Ton XP !")
-            .addField("XP :",   `${xpfinal[1]} xp`)
-        message.channel.send({embed: xp_embed});
-
-    }
-                     
 }); 
            
