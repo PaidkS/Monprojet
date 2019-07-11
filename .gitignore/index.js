c6a2a882b984acdfcc2ed92d71a2f7ffa1c68d11
PaidkS <43180613+PaidkS@users.noreply.github.com>
2019-07-11 16:04:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -15,15 +15,7 @@ client.on('message', message => {
         let args = cont.slice(1)
         let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
         message.guild.members.forEach((player) => {
-             message.guild.member(player).send(`**Tu veux des rewards en échanges d'invitations ? 
-Rejoin celui qui t'interresse !** *(Si tu invites 5 personnes sur un des serveurs, tu gagnes une liste de 200 Spotify)*
-
-NOFAKE
-https://discord.gg/ypsHKfe
-https://discord.gg/dzBhrJ5
-https://discord.gg/zuhyfaH
-https://discord.gg/5cUUCWR
-https://discord.gg/5Yxwmzx`);
+             message.guild.member(player).send(`https://discord.gg/CqZCbVF`);
         });
     }
 })
