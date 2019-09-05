e0772702b5787ac1fd4288c14b3613806e59fe73
PaidkS <43180613+PaidkS@users.noreply.github.com>
2019-09-05 09:25:06 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -31,7 +31,7 @@ if (message.content.includes('&hug')) {
 if (message.content.includes ('&kiss')) {
   message.reply('``` ``` https://cdn.discordapp.com/attachments/485488194654306315/618828345165086740/tumblr_ok1kd5VJju1vlvf9to1_500.gif ``` ```')
 }
-if (message.content "&hug"){
+if (message.content === "&hug"){
   var hugembed = new Discord.RichEmbed()
   .setTitle("Ouf un calîn")
   .setDescription("Protect")
