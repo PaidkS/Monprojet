493bf24bdeaa4c91d3f1813a18924d526d2f39b9
PaidkS <43180613+PaidkS@users.noreply.github.com>
2019-09-05 09:18:14 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -36,6 +36,7 @@ if (message.content.includes("&hug"){
   .setTitle("Ouf un calîn")
   .setDescription("Protect")
   .setColor("#15f153") 
+  .setFooter("ça wlh")
   .setTImage("https://cdn.discordapp.com/attachments/485488194654306315/618828315951759400/tumblr_mx084htXKO1qbvovho1_500.gif") 
   .addField("Nom du Bot", bot.user.username)
   .addField("Crée sur :", bot.user.createdAt);
