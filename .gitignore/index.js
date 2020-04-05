88b36c37c5eb9b2c42c9081044c03737ab5f9f12
PaidkS <43180613+PaidkS@users.noreply.github.com>
2020-04-05 18:29:34 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -22,7 +22,7 @@ bot.on("message", message => {
 
     if (args[0].toLocaleLowerCase() == "!roll"){
       message.reply("*** ")
-      message.reply("*** "+ args[0].toLocaleLowerCase())
+      message.reply("*** "+ args.length())
       var rproll = Math.floor(Math.random() *6) + 1;
       //var rproll = message.content. (" ")
       //message.reply(args[1].trim().toString())
