df5ffee9d747246242d5ce76bb9ebed7574e82cd
PaidkS <43180613+PaidkS@users.noreply.github.com>
2020-04-06 14:40:02 +0200

    Create niv.js

---
+++ .gitignore/niv.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const exp = require('../exp.json');
+
+module.exports.run = async (bot, message, args) => {
+  if (!exp[message.author.id]) {
+    exp[message.author.id] = {
+      exp: 0,
+      niveau 1
+    };
+  }
+  
+  let cExp = exp[message.author.id].exp;
+  let cNiv = exp[message.author.id].Niveau;
+  let nextLevelUp =cNiv * 15;
+  let expNeededForLevelUp = nextLevelUp = cExp;
+  
+  let nivEmbed = new Discord.RichEmbed()
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
+    .setColor('#dc143c')
+    .addField('Niveau :', cNiv, true)
+    .setTimestamp()
+    .setFooter("Naenelis Bot");
+    );
+  
+  message.channel.send(nivEmbed);
+};
+
+module.exports.help = {
+  name: 'niv' 
+};
