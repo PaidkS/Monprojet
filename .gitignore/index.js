6f31d1c86989e9ea08e2eb69bb3bbdb94b062294
PaidkS <43180613+PaidkS@users.noreply.github.com>
2020-04-06 19:53:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,5 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const fs = require("fs");
-let db = JSON.parse(fs.readFileSync("./exp.json"));
 
 
 bot.login (process.env.TOKEN);
@@ -14,40 +12,19 @@ bot.on('guildMemberAdd', member => {
 });
 
 bot.on("message", message => {
-  if (message.author.bot) return; // ignore bots
+  
+  if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
 
-    // if the user is not on db add the user and change his values to 0
-    if (!db[message.author.id]) db[message.author.id] = {
-        xp: 0,
-        level: 0
-      };
-    db[message.author.id].xp++;
-    let userInfo = db[message.author.id];
-    if(userInfo.xp > 100) {
-        userInfo.level++
-        userInfo.xp = 0
-        message.reply("Congratulations, you level up")
+    if (args[0].toLocaleLowerCase() ===  '!kick'){
+        if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.sendMessage("Ptdr t ki ? ")
+        let member = message.mentions.members.first()
+        if (!member) return message.channel.sendMessage("Mentionne un mec là !!!")
+        if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.sendMessage("tu t'es pris pour qui, tu peux pas le kick")
+        member.kick()
+        message.channel.sendMessage("**"+member.user.username + '** à été exclu !')
     }
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    if(cmd === "info") {
-        let userInfo = db[message.author.id];
-        let member = message.mentions.members.first();
-        let embed = new Discord.RichEmbed()
-        .setColor(0x4286f4)
-        .addField("Level", userInfo.level)
-        .addField("XP", userInfo.xp+"/100");
-        if(!member) return message.channel.sendEmbed(embed)
-        let memberInfo = db[member.id]
-        let embed2 = new Discord.RichEmbed()
-        .setColor(0x4286f4)
-        .addField("Level", memberInfo.level)
-        .addField("XP", memberInfo.xp+"/100")
-        message.channel.sendEmbed(embed2)
-    }
-    fs.writeFile("./exp.json", JSON.stringify(db), (x) => {
-        if (x) console.error(x)
-      });    
+
 
     if (args[0].toLocaleLowerCase() === "!roll" && args.length === 2){     
       var rproll = Math.floor(Math.random() *args[1].valueOf()) + 1; 
