25b41036b0da23ffc4b40d85f04d16c99503199d
PaidkS <43180613+PaidkS@users.noreply.github.com>
2020-04-06 18:31:44 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -178,7 +178,7 @@ bot.on("message", message => {
           .setAuthor(message.author.username, message.author.displayAvatarURL)       
           .setColor("#dc143c")
           .setTimestamp()
-          .addField(`commandes :`, "test", True)         
+          .addField('commandes :', 'test', true)         
           .setFooter("Naenelis Bot");
           return message.channel.send(menuembed);      
                     
